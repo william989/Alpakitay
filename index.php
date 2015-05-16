6e14ea0d6d43cfd05d07df8c19b20cbe43ee8045
hwilliamcy <hwilliam.cy@gmail.com>
2015-05-16 17:02:56 -0500

    cambio en index

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$app->register(new Herrera\Pdo\PdoServiceProvider(),
+  array(
+    'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"],
+    'pdo.port' => $dbopts["port"],
+    'pdo.username' => $dbopts["user"],
+    'pdo.password' => $dbopts["pass"]
+  )
+);
+
 <!DOCTYPE html>
 <html>
 <head>
